4a212e2007652d3510de4ba73e6b2b33f3e2cf67
herokufree1 <herokufreeji@gmail.com>
2025-10-10 13:19:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_HASH = os.environ.get("API_HASH", "27609daf8ee847978797a359024e7607")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8062685163:AAGmDV1_EhXWaCbmRL_YFb1SgwJAwWkhd0Q")
 
 OWNER_ID = int(os.environ.get("OWNER_ID", "2073438175"))  # Your Telegram user ID
-#SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "").split()))  # Space-separated user IDs
+SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "").split()))  # Space-separated user IDs
 
 #MONGO_URL = os.environ.get("MONGO_URL", "")##your mongo url eg: withmongodb+srv://xxxxxxx:xxxxxxx@clusterX.xxxx.mongodb.net/?retryWrites=true&w=majority
 #CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-"))  # Telegram channel ID (with -100 prefix)
